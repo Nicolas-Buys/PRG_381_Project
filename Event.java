66cfe18fba9a30045180e1117022afd266b6e6a3
IanDoesCoding <mail4ian41@gmail.com>
2021-02-24 19:06:31 +0200

    Event Constructor/Variables added

--- Event.java
+++ Event.java
@@ -1,8 +1,36 @@
+import java.sql.Date;
 
 public class Event {
 
+public String eventType,address,veneuName,cellNumber,foodSelection,decorations;
+ public String dateT;
+ public int numberOfpeople,numberOfkids,numberOfAdults;// Number of people = number of kids + number of adults 
  
-//Event  Code
+                                 
+ 
+ 
+
+    public Event(String EventType, String Address,String VeneuName,String CellNumber,String FoodSelection,String Decorations,String DateT,int NumberOfpeople,int NumberOfkids,int NumberOfAdults )
+    {
+
+        eventType = EventType;
+        address = Address;
+        veneuName = VeneuName;
+        cellNumber = CellNumber;
+        foodSelection = FoodSelection;
+        decorations = Decorations;                       //  Constructor for event
+        dateT = DateT;
+        numberOfpeople = NumberOfpeople;
+        numberOfkids = NumberOfkids;
+        numberOfAdults = NumberOfAdults;
+
+
+
+    }
+
+
+ 
+
 
 
     
