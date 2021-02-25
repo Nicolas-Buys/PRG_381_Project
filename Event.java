c33c8cfde9d81727e9796f8a8f60f78b2eaf04df
IanDoesCoding <mail4ian41@gmail.com>
2021-02-25 13:40:54 +0200

    Added Properties

--- Event.java
+++ Event.java
@@ -2,11 +2,106 @@ import java.sql.Date;
 
 public class Event {
 
-public String eventType,address,veneuName,cellNumber,foodSelection,decorations;
- public String dateT;
- public int numberOfpeople,numberOfkids,numberOfAdults;// Number of people = number of kids + number of adults 
+ private String eventType,address,veneuName,cellNumber,foodSelection,decorations;
+ private String dateT;
+ private int numberOfpeople,numberOfkids,numberOfAdults;// Number of people = number of kids + number of adults 
  
-                                 
+
+    //Properties
+
+     public String geteventType()
+     {
+        return eventType;
+     }   
+     public void seteventType(String eventType)
+     {
+         this.eventType = eventType;
+     }
+
+     public String address()
+     {
+        return address;
+     }   
+     public void address(String address)
+     {
+         this.address = address;
+     }
+
+     public String veneuName()
+     {
+        return veneuName;
+     }   
+     public void veneuName(String veneuName)
+     {
+         this.veneuName = veneuName;
+     }
+
+     public String cellNumber()
+     {
+        return cellNumber;
+     }   
+     public void cellNumber(String cellNumber)
+     {
+         this.cellNumber = cellNumber;
+     }
+
+     public String foodSelection()
+     {
+        return foodSelection;
+     }   
+     public void foodSelection(String foodSelection)
+     {
+         this.foodSelection = foodSelection;
+     }
+
+     public String decorations()
+     {
+        return decorations;
+     }   
+     public void decorations(String decorations)
+     {
+         this.decorations = decorations;
+     }
+
+     public String dateT()
+     {
+        return dateT;
+     }   
+     public void dateT(String dateT)
+     {
+         this.dateT = dateT;
+     }
+
+     public int numberOfpeople()
+     {
+        return numberOfpeople;
+     }   
+     public void numberOfpeople(int numberOfpeople)
+     {
+         this.numberOfpeople = numberOfpeople;
+     }
+
+     public int numberOfkids()
+     {
+        return numberOfkids;
+     }   
+     public void numberOfkids(int numberOfkids)
+     {
+         this.numberOfkids = numberOfkids;
+     }
+
+     public int numberOfAdults()
+     {
+        return numberOfAdults;
+     }   
+     public void numberOfAdults(int numberOfAdults)
+     {
+         this.numberOfAdults = numberOfAdults;
+     }
+
+
+
+
  
  
 
