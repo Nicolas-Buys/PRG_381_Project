d2c70fc612cf551857dfdf206166d520b2a48cb6
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-02-25 14:12:17 +0200

    Added ClientList

--- Client.java
+++ Client.java
@@ -1,13 +1,49 @@
+import java.util.*;
+
 public class Client {
     String name, surname, cellNumber;
 
+    // Property for name field
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+
+    // property for surname field
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String s){
+        this.surname = s;
+    }
+
+    // porperty for cell number
+    public String getCellNumber(){
+        return cellNumber;
+    }
+    public void setCellNumber(String c){
+        this.cellNumber = c;
+    }
+
+    //constructor for client class
     public Client(String n, String s, String c){
         this.name = n;
         this.surname = s;
         this.cellNumber = c;
     }
 
+    // Empty constructor for creating objects of this class without having parameters
     public Client(){
 
+    }
+
+    public List<Client> GetClient(List<Client> clientList){
+        List<Client> ClientList = new ArrayList<Client>();
+        
+        ClientList = clientList;
+
+        return ClientList;
     }
 }
