4de93e831d28c5f7b9cbda42491a5a16413539c2
Nicolas-Buys <576529@student.belgiumcampus.ac.za>
2021-02-25 16:01:45 +0200

    connection string update

--- DataLinkLayer/Database.java
+++ DataLinkLayer/Database.java
@@ -3,12 +3,18 @@ package DataLinkLayer;
 import java.sql.*;
 public class Database {
     String connectionUrl =
-    "jdbc:sqlserver://yourserver.database.windows.net:1433;"
-            + "database=AdventureWorks;"
-            + "user=yourusername@yourserver;"
-            + "password=yourpassword;"
+    "jdbc:sqlserver://DESKTOP-Q58KIN3.database.windows.net:1433;"
+            + "database=Catering;"
+            + "user=DESKTOP-Q58KIN3\\Nicol@DESKTOP-Q58KIN3;"
+            + "password=u9581abc;"
             + "encrypt=true;"
             + "trustServerCertificate=false;"
             + "loginTimeout=30;";
+    try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            // Code here.
+        }
+        // Handle any errors that may have occurred.
+    catch (SQLException e) {
+        e.printStackTrace();
 
 }
