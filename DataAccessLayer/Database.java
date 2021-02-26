4529fcc7c7836810930431ae4aae1bad66ee1b50
Nicolas-Buys <576529@student.belgiumcampus.ac.za>
2021-02-26 10:47:47 +0200

    Adding database driver and updating connectionstr

--- DataAccessLayer/Database.java
+++ DataAccessLayer/Database.java
@@ -3,13 +3,9 @@ package DataAccessLayer;
 import java.sql.*;
 public class Database {
     String connectionUrl =
-    "jdbc:sqlserver://DESKTOP-Q58KIN3.database.windows.net:1433;"
-            + "database=Catering;"
-            + "user=DESKTOP-Q58KIN3\\Nicol@DESKTOP-Q58KIN3;"
-            + "password=u9581abc;"
-            + "encrypt=true;"
-            + "trustServerCertificate=false;"
-            + "loginTimeout=30;"; 
+    "jdbc:sqlserver://105.186.136.224:1433;"
+            + "databaseName=Catering;"
+            + "integratedSecurity=true;" ; 
     ResultSet resultSet = null;
     public void Databasetest() {
         try (Connection connection = DriverManager.getConnection(connectionUrl);
