4f5f936253439eea17949cd5f2560a87aad8d5f1
Nicolas-Buys <576529@student.belgiumcampus.ac.za>
2021-02-26 20:22:51 +0200

    Changing connectionstring to work properly

--- DataAccessLayer/Database.java
+++ DataAccessLayer/Database.java
@@ -2,26 +2,28 @@ package DataAccessLayer;
 
 import java.sql.*;
 public class Database {
-    String connectionUrl =
-    "jdbc:sqlserver://105.186.136.224:1433;"
-            + "databaseName=Catering;"
-            + "integratedSecurity=true;" ; 
+    String connectionUrl = "jdbc:sqlserver://105.186.136.224:1433;"
+    + "instanceName=mssqlserver;"
+    + "user=sa;"
+    + "password=PRG381Project;"
+    + "databaseName=Catering";
     ResultSet resultSet = null;
     public void Databasetest() {
         try (Connection connection = DriverManager.getConnection(connectionUrl);
         Statement statement = connection.createStatement();) {
             // Code here.
-            String selectSql = "SELECT * from dbo.AdultMeals";
+            String selectSql = "SELECT TOP 10 * from [dbo].[AdultMeals]";
             resultSet = statement.executeQuery(selectSql);
             // Print results from select statement
             while (resultSet.next()) {
                 System.out.println(resultSet.getString(2) + " " + resultSet.getString(3));
+            }   
         }
-    }
         // Handle any errors that may have occurred.
-    catch (SQLException e) {
-        System.out.println("its not working");
-    }
+        catch (SQLException e) {
+            System.out.println("its not working");
+            e.printStackTrace();
+        }
     }
     
 }
