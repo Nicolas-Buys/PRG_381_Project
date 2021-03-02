c20ca86cd65170e3f3696519c745e080905f341c
Hendrik-19 <576326@student.belgiumcampus.ac.za>
2021-03-02 16:14:31 +0200

    removed unused methods

--- DataAccessLayer/Database.java
+++ DataAccessLayer/Database.java
@@ -44,11 +44,7 @@ public class Database {
             }     
     }    
 
-    public void getListofEvent(List<Event> el){
-        
-        List<Event> eventList = new ArrayList<>();
-        eventList =el;
-    }
+    
     
 }
 
